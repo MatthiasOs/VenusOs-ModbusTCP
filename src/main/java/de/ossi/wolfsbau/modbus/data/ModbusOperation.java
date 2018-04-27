8be52f469b1f3719ed0acae83ec801b693197eff
CommentSectionScientist <matthias-oswald@gmx.net>
2018-04-27 20:18:19 +0200

    Neue ModbusOperationen. Scalefactor auf double umgestellt.

--- src/main/java/de/ossi/wolfsbau/modbus/data/ModbusOperation.java
+++ src/main/java/de/ossi/wolfsbau/modbus/data/ModbusOperation.java
@@ -2,12 +2,25 @@ package de.ossi.wolfsbau.modbus.data;
 
 import org.apache.commons.lang3.Range;
 
+/**
+ * Unterstützte Modbus Operationen nach dem Excel Sheet:
+ * CCGX-Modbus-TCP-register-list-2.12.xlsx 
+ * @author ossi
+ *
+ */
 public class ModbusOperation {
 
 	public static final ModbusOperation AC_CONSUMPTION_L1 = new ModbusOperation(817, "AC Consumption L1", 1, DBusUnit.W, Range.between(0, 65336));
 	public static final ModbusOperation AC_CONSUMPTION_L2 = new ModbusOperation(818, "AC Consumption L2", 1, DBusUnit.W, Range.between(0, 65336));
 	public static final ModbusOperation AC_CONSUMPTION_L3 = new ModbusOperation(819, "AC Consumption L3", 1, DBusUnit.W, Range.between(0, 65336));
-	public static final ModbusOperation BATTERY_STATE_OF_CHARGE = new ModbusOperation(843, "Battery State of Charge (System)", 1, DBusUnit.PERCENT, Range.between(0, 100));
+	public static final ModbusOperation ACTIVE_INPUT_SOURCE = new ModbusOperation(826, "Active input source", 1, DBusUnit.SOURCE, Range.between(0, 32668));
+	public static final ModbusOperation BATTERY_VOLTAGE_SYSTEM = new ModbusOperation(840, "Battery Voltage (System)", 10, DBusUnit.V_DC, Range.between(0, 65336));
+	public static final ModbusOperation BATTERY_CURRENT_SYSTEM = new ModbusOperation(841, "Battery Current (System)", 10, DBusUnit.V_DC, Range.between(-32668, 32668));
+	public static final ModbusOperation BATTERY_POWER_SYSTEM = new ModbusOperation(842, "Battery Power (System)", 1, DBusUnit.W, Range.between(-32668, 32668));
+	public static final ModbusOperation BATTERY_STATE_OF_CHARGE_SYSTEM = new ModbusOperation(843, "Battery State of Charge (System)", 1, DBusUnit.PERCENT, Range.between(0, 100));
+	public static final ModbusOperation BATTERY_STATE_SYSTEM = new ModbusOperation(844, "Battery state (System)", 1, DBusUnit.BATTERY_STATE, Range.between(0, 65336));
+	public static final ModbusOperation BATTERY_CONSUMED_SYSTEM = new ModbusOperation(845, "Battery Consumed Amphours (System)", -10, DBusUnit.AH, Range.between(-65336, 0));
+	public static final ModbusOperation BATTERY_TIME_TO_GO_SYSTEM = new ModbusOperation(846, "Battery Time to Go (System)", 0.01, DBusUnit.S, Range.between(0, 6533600));
 	// Carlo Gavazzi Meter direct
 	public static final ModbusOperation GRID_L1_POWER = new ModbusOperation(2600, "Grid L1 Power", 1, DBusUnit.W, Range.between(-32668, 32668));
 	public static final ModbusOperation GRID_L2_POWER = new ModbusOperation(2601, "Grid L2 Power", 1, DBusUnit.W, Range.between(-32668, 32668));
@@ -19,11 +32,11 @@ public class ModbusOperation {
 
 	private final int address;
 	private final String description;
-	private final int scaleFactor;
+	private final double scaleFactor;
 	private final DBusUnit dbusUnit;
 	private final Range<Integer> wertRange;
 
-	private ModbusOperation(int address, String description, int scaleFactor, DBusUnit dbusUnit, Range<Integer> wertRange) {
+	private ModbusOperation(int address, String description, double scaleFactor, DBusUnit dbusUnit, Range<Integer> wertRange) {
 		this.address = address;
 		this.description = description;
 		this.scaleFactor = scaleFactor;
@@ -44,7 +57,7 @@ public class ModbusOperation {
 		return description;
 	}
 
-	public int getScaleFactor() {
+	public double getScaleFactor() {
 		return scaleFactor;
 	}
 
