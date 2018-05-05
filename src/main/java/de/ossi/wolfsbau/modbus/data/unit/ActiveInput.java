0d35eff9296e0e2fef2e87cf6a42b2a2e18ec17d
CommentSectionScientist <36775764+CommentSectionScientist@users.noreply.github.com>
2018-05-05 17:19:11 +0200

    Korrigiert ActiveInput Einheit (#32)

--- src/main/java/de/ossi/wolfsbau/modbus/data/unit/ActiveInput.java
+++ src/main/java/de/ossi/wolfsbau/modbus/data/unit/ActiveInput.java
@@ -4,8 +4,8 @@ package de.ossi.wolfsbau.modbus.data.unit;
 public class ActiveInput extends DBusUnit {
 	public ActiveInput(String name) {
 		super(name);
-		values.put(0, "AC Input");
-		values.put(1, "AC Input");
-		values.put(240, "Not Connected");
+		values.put(0, "AC Input 1");
+		values.put(1, "AC Input 2");
+		values.put(240, "Disconnected");
 	}
 }
