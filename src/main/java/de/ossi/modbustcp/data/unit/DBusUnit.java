711e508ad76a17403991ab654dfb8b5e3b8920fb
CommentSectionScientist <matthias-oswald@gmx.net>
2019-11-06 19:48:02 +0100

    New Operations with new Units (3rd Revision -> Excel) Some lambda Expressions

--- src/main/java/de/ossi/modbustcp/data/unit/DBusUnit.java
+++ src/main/java/de/ossi/modbustcp/data/unit/DBusUnit.java
@@ -9,6 +9,8 @@ import lombok.ToString;
 public class DBusUnit implements Serializable {
 
 	private static final long serialVersionUID = 1496887712045628303L;
+	
+	//Normal units >>>
 	public static final DBusUnit V = new DBusUnit("V");
 	public static final DBusUnit V_AC = new DBusUnit("V AC");
 	public static final DBusUnit V_DC = new DBusUnit("V DC");
@@ -23,6 +25,24 @@ public class DBusUnit implements Serializable {
 	public static final DBusUnit AH = new DBusUnit("Ah");
 	public static final DBusUnit S = new DBusUnit("s");
 	public static final DBusUnit CELSIUS = new DBusUnit("Degrees celsius");
+	//Do we really need a count unit? Maybe an empty unit instead?
+	public static final DBusUnit COUNT = new DBusUnit("Count");
+	
+	//Special units with values >>>
+	public static final DBusUnit STATE = new DBusSpecialUnit("State")
+			.withValue(0, "Off")
+			.withValue(1, "Low Power")
+			.withValue(2, "Fault")
+			.withValue(3, "Bulk")
+			.withValue(4, "Absorption")
+			.withValue(5, "Float")
+			.withValue(6, "Storage")
+			.withValue(7, "Equalize")
+			.withValue(8, "Passthru")
+			.withValue(9, "Inverting")
+			.withValue(10, "Power assist")
+			.withValue(11, "Power supply")
+			.withValue(252, "Bulk protection");
 	public static final DBusUnit ACTIVE_INPUT = new DBusSpecialUnit("Active Input")
 			.withValue(0, "AC Input 1")
 			.withValue(1, "AC Input 2")
@@ -90,6 +110,24 @@ public class DBusUnit implements Serializable {
 			.withValue(2, "Inverter Only")
 			.withValue(3, "On")
 			.withValue(4, "Off");
+	public static final DBusUnit ERROR = new DBusSpecialUnit("Error")
+			.withValue(0, "No error")
+			.withValue(1, "Device is switched off because one of the other phases in the system has switched off")
+			.withValue(2, "New and old types MK2 are mixed in the system")
+			.withValue(3, "Not all- or more than- the expected devices were found in the system")
+			.withValue(4, "No other device whatsoever detected")
+			.withValue(5, "Overvoltage on AC-out")
+			.withValue(6, "Error in DDC Program")
+			.withValue(7, "VE.Bus BMS connected- which requires an Assistant- but no assistant found")
+			.withValue(10, "System time synchronisation problem occurred")
+			.withValue(14, "Device cannot transmit data")
+			.withValue(16, "Dongle missing")
+			.withValue(17, "One of the devices assumed master status because the original master failed")
+			.withValue(18, "AC Overvoltage on the output of a slave has occurred while already switched off")
+			.withValue(22, "This device cannot function as slave")
+			.withValue(24, "Switch-over system protection initiated")
+			.withValue(25, "Firmware incompatibility. The firmware of one of the connected device is not sufficiently up to date to operate in conjunction with this device")
+			.withValue(26, "Internal error");
 
 	@Getter
 	private final String name;
