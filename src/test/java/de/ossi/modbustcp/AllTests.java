021987596dabfd0415ad69e34f158a5eb9ae8fda
CommentSectionScientist <36775764+CommentSectionScientist@users.noreply.github.com>
2020-04-12 20:54:22 +0200

    Removes ALL unused INVERTER XML and SQL-Lite DB Operations (#79)
    Co-authored-by: CommentSectionScientist <matthias-oswald@gmx.net>

--- src/test/java/de/ossi/modbustcp/AllTests.java
+++ src/test/java/de/ossi/modbustcp/AllTests.java
@@ -11,16 +11,10 @@ import de.ossi.modbustcp.data.operation.ModbusOperationSolarTest;
 import de.ossi.modbustcp.data.operation.ModbusOperationSystemTest;
 import de.ossi.modbustcp.data.operation.ModbusOperationTemperatureTest;
 import de.ossi.modbustcp.data.operation.ModbusOperationVebusTest;
-import de.ossi.modbustcp.db.DBModelTest;
-import de.ossi.modbustcp.db.util.EnumPairTest;
-import de.ossi.modbustcp.db.util.XMLtoDBConverterTest;
-import de.ossi.modbustcp.inverter.InverterRequesterTest;
-import de.ossi.modbustcp.parser.WRAntwortParserTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ WRAntwortParserTest.class, DBModelTest.class, InverterRequesterTest.class, EnumPairTest.class, XMLtoDBConverterTest.class, ModbusOperationSystemTest.class,
-		ModbusOperationGridTest.class, ModbusOperationVebusTest.class, ModbusOperationBatteryTest.class, ModbusOperationSolarTest.class, ModbusOperationTemperatureTest.class,
-		ModbusResultIntTest.class })
+@SuiteClasses({ ModbusOperationSystemTest.class, ModbusOperationGridTest.class, ModbusOperationVebusTest.class, ModbusOperationBatteryTest.class, ModbusOperationSolarTest.class,
+		ModbusOperationTemperatureTest.class, ModbusResultIntTest.class })
 public class AllTests {
 
 }
