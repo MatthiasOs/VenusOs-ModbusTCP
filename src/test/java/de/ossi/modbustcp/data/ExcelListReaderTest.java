567bc09266c17024393358f4de573a624012cd68
CommentSectionScientist <matthias-oswald@gmx.net>
2022-12-27 12:56:14 +0100

    Junit-Tests fixt maven surefire plugin

--- src/test/java/de/ossi/modbustcp/data/ExcelListReaderTest.java
+++ src/test/java/de/ossi/modbustcp/data/ExcelListReaderTest.java
@@ -1,48 +1,40 @@
 package de.ossi.modbustcp.data;
 
 
-import java.util.List;
-
-
 import de.ossi.modbustcp.data.operation.ModbusDevice;
 import de.ossi.modbustcp.data.operation.ModbusOperation;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Disabled("Cant find Excel File in Resource Folder")
 public class ExcelListReaderTest {
-	private static final String OPERATIONS_DEVICES_FILENAME = "CCGX-Modbus-TCP-register-list-2.53.xlsx";
-	private static ExcelListReader reader;
+    private static final String OPERATIONS_DEVICES_FILENAME = "CCGX-Modbus-TCP-register-list-2.53.xlsx";
+    private final ExcelListReader reader = new ExcelListReader(OPERATIONS_DEVICES_FILENAME);
 
-	@BeforeAll static void setUp(){
-		reader = new ExcelListReader(ExcelListReaderTest.class.getClassLoader().getResource(OPERATIONS_DEVICES_FILENAME).getFile());
-	}
+    @Test
+    public void operationsShouldBeRead() {
+        List<ModbusOperation> operations = reader.readOperations();
+        assertThat(operations).isNotEmpty();
+    }
 
-	@Test
-	public void operationsShouldBeRead() throws Exception {
-		List<ModbusOperation> operations = reader.readOperations();
-		assertThat(operations).isNotEmpty();
-	}
+    @Test
+    public void operationsShouldBeTheExpectedAmount() {
+        List<ModbusOperation> operations = reader.readOperations();
+        assertThat(operations).hasSize(343);
+    }
 
-	@Test
-	public void operationsShouldBeTheExpectedAmount() throws Exception {
-		List<ModbusOperation> operations = reader.readOperations();
-		assertThat(operations).hasSize(343);
-	}
+    @Test
+    public void devicesShouldBeRead() {
+        List<ModbusDevice> devices = reader.readDevices();
+        assertThat(devices).isNotEmpty();
+    }
 
-	@Test
-	public void devicesShouldBeRead() throws Exception {
-		List<ModbusDevice> devices = reader.readDevices();
-		assertThat(devices).isNotEmpty();
-	}
-
-	@Test
-	public void devicesShouldBeTheExpectedAmount() throws Exception {
-		List<ModbusDevice> devices = reader.readDevices();
-		assertThat(devices).hasSize(57);
-	}
+    @Test
+    public void devicesShouldBeTheExpectedAmount() {
+        List<ModbusDevice> devices = reader.readDevices();
+        assertThat(devices).hasSize(57);
+    }
 
 }
--- src/test/java/de/ossi/modbustcp/data/ModbusResultIntTest.java
+++ src/test/java/de/ossi/modbustcp/data/ModbusResultIntTest.java
@@ -1,51 +1,49 @@
 package de.ossi.modbustcp.data;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-
 import de.ossi.modbustcp.data.operation.ModbusDevice;
 import de.ossi.modbustcp.data.operation.ModbusOperation;
 import de.ossi.modbustcp.data.unit.AccessMode;
 import de.ossi.modbustcp.data.unit.Category;
 import de.ossi.modbustcp.data.unit.DBusUnit;
 import de.ossi.modbustcp.data.unit.Type;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("When ModbusResultInt")
 public class ModbusResultIntTest {
 
-	private static final DBusUnit V_AC = new DBusUnit("V AC");
-	private static final DBusUnit BATTERYLIFE_STATE = new DBusUnit("0=Unused, BL disabled;1=Restarting;2=Self-consumption;3=Self-consumption;4=Self-consumption;5=Discharge");
-	private static final ModbusDevice CAN_BUS_BMS = new ModbusDevice(225, 512, "CAN-bus BMS");
-	private static final ModbusOperation SET_ESS_BATTERY_LIFE_STATE = new ModbusOperation(Category.SETTINGS, "ESS BatteryLife state", 2900, Type.UINT16, 1D, "",
-			"/Settings/CGwacs/BatteryLife/State", AccessMode.READ_WRITE, BATTERYLIFE_STATE, "");
-	private static final ModbusOperation GRI_GRID_L1_VOLTAGE = new ModbusOperation(Category.GRID, "Grid L1 – Voltage", 2616, Type.UINT32, 10D, "", "/Ac/L1/Voltage",
-			AccessMode.READ_ONLY, V_AC, "");
-	private ModbusResultInt modbusResult;
+    private static final DBusUnit V_AC = new DBusUnit("V AC");
+    private static final DBusUnit BATTERYLIFE_STATE = new DBusUnit("0=Unused, BL disabled;1=Restarting;2=Self-consumption;3=Self-consumption;4=Self-consumption;5=Discharge");
+    private static final ModbusDevice CAN_BUS_BMS = new ModbusDevice(225, 512, "CAN-bus BMS");
+    private static final ModbusOperation SET_ESS_BATTERY_LIFE_STATE = new ModbusOperation(Category.SETTINGS, "ESS BatteryLife state", 2900, Type.UINT16, 1D, "",
+            "/Settings/CGwacs/BatteryLife/State", AccessMode.READ_WRITE, BATTERYLIFE_STATE, "");
+    private static final ModbusOperation GRI_GRID_L1_VOLTAGE = new ModbusOperation(Category.GRID, "Grid L1 – Voltage", 2616, Type.UINT32, 10D, "", "/Ac/L1/Voltage",
+            AccessMode.READ_ONLY, V_AC, "");
+    private ModbusResultInt modbusResult;
 
-	@DisplayName("Parsing Special Unit")
-	@Nested
-	class ParsingSpecialUnit {
-		@DisplayName("BATTERYLIFE_STATE")
-		@Test
-		void test1() throws Exception {
-			modbusResult = new ModbusResultInt(SET_ESS_BATTERY_LIFE_STATE, CAN_BUS_BMS, 1);
-			assertThat(modbusResult.getValueOfOperationWithUnit()).isEqualTo("Restarting");
-		}
-	}
+    @DisplayName("Parsing Special Unit")
+    @Nested
+    class ParsingSpecialUnit {
+        @DisplayName("BATTERYLIFE_STATE")
+        @Test
+        void test1() {
+            modbusResult = new ModbusResultInt(SET_ESS_BATTERY_LIFE_STATE, CAN_BUS_BMS, 1);
+            assertThat(modbusResult.getValueOfOperationWithUnit()).startsWith("Restarting");
+        }
+    }
 
-	@DisplayName("Parsing Unit")
-	@Nested
-	class ParsingUnit {
+    @DisplayName("Parsing Unit")
+    @Nested
+    class ParsingUnit {
 
-		@DisplayName("V_AC")
-		@Test
-		void test1() throws Exception {
-			modbusResult = new ModbusResultInt(GRI_GRID_L1_VOLTAGE, CAN_BUS_BMS, 1);
-			assertThat(modbusResult.getValueOfOperationWithUnit()).isEqualTo("0.1 V AC");
-		}
-	}
+        @DisplayName("V_AC")
+        @Test
+        void test1() {
+            modbusResult = new ModbusResultInt(GRI_GRID_L1_VOLTAGE, CAN_BUS_BMS, 1);
+            assertThat(modbusResult.getValueOfOperationWithUnit()).isEqualTo("0.1 V AC");
+        }
+    }
 }
