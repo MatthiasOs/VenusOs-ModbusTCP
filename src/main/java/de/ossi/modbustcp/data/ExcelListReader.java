a81bee3a95b478b53091537c6799afe58f1a5a8d
CommentSectionScientist <matthias-oswald@gmx.net>
2022-12-28 18:10:28 +0100

    Changed URL to InputStream Reading of File works now when executing the jar

--- src/main/java/de/ossi/modbustcp/data/ExcelListReader.java
+++ src/main/java/de/ossi/modbustcp/data/ExcelListReader.java
@@ -1,48 +1,56 @@
 package de.ossi.modbustcp.data;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.net.URL;
+import com.creditdatamw.zerocell.Reader;
+import de.ossi.modbustcp.data.operation.ModbusDevice;
+import de.ossi.modbustcp.data.operation.ModbusOperation;
+import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.creditdatamw.zerocell.Reader;
-
-import de.ossi.modbustcp.data.operation.ModbusDevice;
-import de.ossi.modbustcp.data.operation.ModbusOperation;
-
 /**
  * Reads in the Data from the Excel Sheet CCGX-Modbus-TCP-register-list
- * 
- * @author ossi
  *
+ * @author ossi
  */
+@RequiredArgsConstructor
 public class ExcelListReader {
 
-	private File file;
+    private final String fileName;
 
-	private static final String OPERATIONS_SHEET_NAME = "Field list";
-	private static final String DEVICES_SHEET_NAME = "Unit ID mapping";
+    private static final String OPERATIONS_SHEET_NAME = "Field list";
+    private static final String DEVICES_SHEET_NAME = "Unit ID mapping";
 
-	public ExcelListReader(String fileName) {
-		if (fileName != null) {
-			URL resource = ExcelListReader.class.getClassLoader().getResource(fileName);
-			if(resource==null){
-				throw new IllegalArgumentException("Datei nicht gefunden: "+fileName);
-			}
-			file = new File(resource.getFile());
-		}
-	}
+    public List<ModbusOperation> readOperations() {
+        try (InputStream inputStream = getInputStream()) {
+            List<ModbusOperation> operations = Reader.of(ModbusOperation.class).from(inputStream).sheet(OPERATIONS_SHEET_NAME).list();
+            // Filter not parseable rows
+            return operations.stream().filter(o -> o.getAddress() != null).collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public List<ModbusOperation> readOperations() {
-		List<ModbusOperation> operations = Reader.of(ModbusOperation.class).from(file).sheet(OPERATIONS_SHEET_NAME).list();
-		// Filter not parseable rows
-		return operations.stream().filter(o -> o.getAddress() != null).collect(Collectors.toList());
-	}
+    public List<ModbusDevice> readDevices() {
+        try (InputStream inputStream = getInputStream()) {
+            List<ModbusDevice> devices = Reader.of(ModbusDevice.class).from(inputStream).sheet(DEVICES_SHEET_NAME).list();
+            // Filter not parseable rows
+            return devices.stream().filter(d -> d.getUnitId() != null).collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public List<ModbusDevice> readDevices() {
-		List<ModbusDevice> devices = Reader.of(ModbusDevice.class).from(file).sheet(DEVICES_SHEET_NAME).list();
-		// Filter not parseable rows
-		return devices.stream().filter(d -> d.getUnitId() != null).collect(Collectors.toList());
-	}
+    private InputStream getInputStream() {
+        if (fileName == null) {
+            throw new IllegalArgumentException("Datei kann nicht leer sein!");
+        }
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName);
+        if (inputStream == null) {
+            throw new IllegalArgumentException(String.format("Datei [%s] nicht gefunden!", fileName));
+        }
+        return inputStream;
+    }
 }
--- src/main/java/de/ossi/modbustcp/data/unit/AccessMode.java
+++ src/main/java/de/ossi/modbustcp/data/unit/AccessMode.java
@@ -1,19 +1,19 @@
 package de.ossi.modbustcp.data.unit;
 
-import java.util.Arrays;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Arrays;
+
 @AllArgsConstructor
 public enum AccessMode {
-	READ_WRITE("yes"), 
-	READ_ONLY("no");
+    READ_WRITE("yes"),
+    READ_ONLY("no");
 
-	@Getter
-	String name;
+    @Getter
+    private final String name;
 
-	public static AccessMode from(String name) {
-		return Arrays.stream(AccessMode.values()).filter(a -> a.getName().equals(name)).findFirst().orElse(null);
-	}
+    public static AccessMode from(String name) {
+        return Arrays.stream(AccessMode.values()).filter(a -> a.getName().equals(name)).findFirst().orElse(null);
+    }
 }
