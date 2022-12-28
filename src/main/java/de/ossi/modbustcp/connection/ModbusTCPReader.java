76e0873e1b48c1050354a75162f5215ca0325977
CommentSectionScientist <matthias-oswald@gmx.net>
2022-12-28 18:30:25 +0100

    Future

--- src/main/java/de/ossi/modbustcp/connection/ModbusTCPReader.java
+++ src/main/java/de/ossi/modbustcp/connection/ModbusTCPReader.java
@@ -1,24 +1,33 @@
 package de.ossi.modbustcp.connection;
 
 import com.ghgande.j2mod.modbus.ModbusException;
-
 import de.ossi.modbustcp.data.ModbusResultInt;
 import de.ossi.modbustcp.data.operation.ModbusDevice;
 import de.ossi.modbustcp.data.operation.ModbusOperation;
 
-public class ModbusTCPReader extends ModbusReaderConnectionHandler {
+import java.util.concurrent.*;
 
-	public ModbusTCPReader(String ip, int port) {
-		super(ip, port);
-	}
+public class ModbusTCPReader extends ModbusConnectionHandler {
 
-	@Override
-	protected ModbusResultInt readInternal(ModbusOperation operation, ModbusDevice device) throws ModbusException {
-		int result = readOperationFromDeviceInternal(operation, device);
-		return new ModbusResultInt(operation, device, result);
-	}
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
-	private int readOperationFromDeviceInternal(ModbusOperation operation, ModbusDevice device) throws ModbusException {
-		return modbusMaster.readMultipleRegisters(device.getUnitId(), operation.getAddress(), 1)[0].getValue();
-	}
+    public ModbusTCPReader(String ip, int port) {
+        super(ip, port);
+    }
+
+    public ModbusResultInt readOperationFromDevice(ModbusOperation operation, ModbusDevice device) throws ModbusException {
+        connect();
+        try {
+            int result = readOperationFromDeviceInternal(operation, device).get(10L, TimeUnit.SECONDS);
+            return new ModbusResultInt(operation, device, result);
+        } catch (ExecutionException | InterruptedException | TimeoutException e) {
+            throw new RuntimeException(e);
+        } finally {
+            disconnect();
+        }
+    }
+
+    private Future<Integer> readOperationFromDeviceInternal(ModbusOperation operation, ModbusDevice device) {
+        return executor.submit(() -> modbusMaster.readMultipleRegisters(device.getUnitId(), operation.getAddress(), 1)[0].getValue());
+    }
 }
