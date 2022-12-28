d468c1a19d990426045e77e1b45a350c6bec02eb
CommentSectionScientist <36775764+CommentSectionScientist@users.noreply.github.com>
2022-12-28 18:11:04 +0100

    Changed URL to InputStream (#96)
    Reading of File works now when executing the jar
    
    Co-authored-by: CommentSectionScientist <matthias-oswald@gmx.net>

--- src/main/java/de/ossi/modbustcp/data/unit/Category.java
+++ src/main/java/de/ossi/modbustcp/data/unit/Category.java
@@ -1,36 +1,36 @@
 package de.ossi.modbustcp.data.unit;
 
-import java.util.Arrays;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Arrays;
+
 @AllArgsConstructor
 public enum Category {
-	SYSTEM("com.victronenergy.system"),
-	VEBUS("com.victronenergy.vebus"), 
-	BATTERY("com.victronenergy.battery"), 
-	SOLARCHARGER("com.victronenergy.solarcharger"), 
-	PVINVERTER("com.victronenergy.pvinverter"), 
-	MOTODRIVE("com.victronenergy.motordrive"),
-	CHARGER("com.victronenergy.charger"),
-	GRID("com.victronenergy.grid"), 
-	SETTINGS("com.victronenergy.settings"), 
-	GPS("com.victronenergy.gps"),
-	TANK("com.victronenergy.tank"),
-	INVERTER("com.victronenergy.inverter"),
-	GENSET("com.victronenergy.genset"), 
-	TEMPERATURE("com.victronenergy.temperature"),
-	PULSEMETER("com.victronenergy.pulsemeter"),
-	DIGITALINPUT("com.victronenergy.digitalinput"),
-	GENERATOR("com.victronenergy.generator"),
-	METEO("com.victronenergy.meteo"),
-	;
-	
-	@Getter
-	String name;
+    SYSTEM("com.victronenergy.system"),
+    VEBUS("com.victronenergy.vebus"),
+    BATTERY("com.victronenergy.battery"),
+    SOLARCHARGER("com.victronenergy.solarcharger"),
+    PVINVERTER("com.victronenergy.pvinverter"),
+    MOTODRIVE("com.victronenergy.motordrive"),
+    CHARGER("com.victronenergy.charger"),
+    GRID("com.victronenergy.grid"),
+    SETTINGS("com.victronenergy.settings"),
+    GPS("com.victronenergy.gps"),
+    TANK("com.victronenergy.tank"),
+    INVERTER("com.victronenergy.inverter"),
+    GENSET("com.victronenergy.genset"),
+    TEMPERATURE("com.victronenergy.temperature"),
+    PULSEMETER("com.victronenergy.pulsemeter"),
+    DIGITALINPUT("com.victronenergy.digitalinput"),
+    GENERATOR("com.victronenergy.generator"),
+    METEO("com.victronenergy.meteo"),
+    ;
 
-	public static Category from(String name) {
-		return Arrays.stream(Category.values()).filter(a -> a.getName().equals(name)).findFirst().orElse(null);
-	}
+    @Getter
+    private final String name;
+
+    public static Category from(String name) {
+        return Arrays.stream(Category.values()).filter(a -> a.getName().equals(name)).findFirst().orElse(null);
+    }
 }
--- src/main/java/de/ossi/modbustcp/data/unit/Type.java
+++ src/main/java/de/ossi/modbustcp/data/unit/Type.java
@@ -1,28 +1,28 @@
 package de.ossi.modbustcp.data.unit;
 
-import java.util.Arrays;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.Arrays;
 
 @AllArgsConstructor
 public enum Type {
 
-	INT16("int16"), 
-	INT32("int32"), 
-	UINT16("uint16"), 
-	UINT32("uint32"), 
-	STRING7("string[7]"), 
-	STRING6("string[6]");
+    INT16("int16"),
+    INT32("int32"),
+    UINT16("uint16"),
+    UINT32("uint32"),
+    STRING7("string[7]"),
+    STRING6("string[6]");
 
-	@Getter
-	String name;
+    @Getter
+    private final String name;
 
-	public static Type from(String name) {
-		return Arrays.stream(Type.values()).filter(a -> a.getName().equals(name)).findFirst().orElse(null);
-	}
+    public static Type from(String name) {
+        return Arrays.stream(Type.values()).filter(a -> a.getName().equals(name)).findFirst().orElse(null);
+    }
 
-	public boolean isUnsigned() {
-		return name.startsWith("u");
-	}
+    public boolean isUnsigned() {
+        return name.startsWith("u");
+    }
 }
